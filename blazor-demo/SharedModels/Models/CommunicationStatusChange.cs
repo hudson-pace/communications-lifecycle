d079165ac05e184bf862382433549e692db6a2c1
Hudson Pace <hudsonmpace@gmail.com>
2025-07-30 17:18:52 -0700

    Add id fields to status changes.

--- blazor-demo/SharedModels/Models/CommunicationStatusChange.cs
+++ blazor-demo/SharedModels/Models/CommunicationStatusChange.cs
@@ -8,9 +8,11 @@ public class CommunicationStatusChange
 {
   public int Id { get; set; }
 
+  public int CommunicationId { get; set; }
   [Required(ErrorMessage = "Communication is required.")]
   public Communication Communication { get; set; } = null!;
 
+  public int CommunicationStatusId { get; set; }
   [Required(ErrorMessage = "Status is required.")]
   public CommunicationStatus Status { get; set; } = null!;
 
