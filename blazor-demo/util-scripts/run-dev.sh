ca88a53fb62dc75227f8065464663ad5bc6c30a8
Hudson Pace <hudsonmpace@gmail.com>
2025-07-31 18:12:33 -0700

    Fix docker build.

--- blazor-demo/util-scripts/run-dev.sh
+++ blazor-demo/util-scripts/run-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build --target dev -t counter-image -f ./BlazorApp1/dockerfile .
-docker build --target prod -t movie-api-image -f ./MoviesApi/dockerfile .
+docker build --target dev -t counter-image -f ./BlazorApp1/Dockerfile .
+docker build --target prod -t movie-api-image -f ./MoviesApi/Dockerfile .
 docker compose -p blazor-app -f compose.yaml -f compose.override.yaml up
