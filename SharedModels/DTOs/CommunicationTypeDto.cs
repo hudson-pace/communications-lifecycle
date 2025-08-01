c67af018f8b99bad17fd5d240dcc972995a3e254
Hudson Pace <hudsonmpace@gmail.com>
2025-08-01 11:38:25 -0700

    TEMP FOR REAL UNDO THIS

--- SharedModels/DTOs/CommunicationTypeDto.cs
+++ SharedModels/DTOs/CommunicationTypeDto.cs
@@ -8,6 +8,8 @@ public class CommunicationTypeDto
 {
   public int Id { get; set; }
 
+  [Required(ErrorMessage = "Name is required.")]
+  [StringLength(20, MinimumLength = 1, ErrorMessage = "Name must be between 1 and 20 characters.")]
   public string Name { get; set; } = null!;
 
   public List<CommunicationStatusDto> Statuses { get; set; } = [];
